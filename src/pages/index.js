c2091e160ba3e3912bda0c40389f2e120acb4565
Karl Tiama <karltiama@gmail.com>
2023-02-13 15:32:11 -0500

    feat: add first post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
+      <Hero />
+      
     </>
   )
 }
