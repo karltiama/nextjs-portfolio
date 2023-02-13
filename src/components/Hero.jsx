e0ba65cdd7474417fbf6f4426f00719180a4a568
Karl Tiama <karltiama@gmail.com>
2023-02-13 15:30:40 -0500

    feat: add Hero Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex justify-between items-center max-w-[1240px] mx-auto px-4 text-3xl h-screen bg-slate-600'>
+      <h1>Testing 123</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Vel totam distinctio dolor? Inventore, nemo ipsam sapiente hic itaque 
+        ut rem officia nulla voluptas repellat ea, dolorem obcaecati exercitationem? 
+        Nobis, excepturi?
+      </p>
+    </div>
+  )
+}
+
+export default Hero
