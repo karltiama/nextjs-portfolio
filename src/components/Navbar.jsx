404c8e88520c91da95ce9457c93d4e4b666b59a4
Karl Tiama <karltiama@gmail.com>
2023-02-13 15:28:59 -0500

    update navbar with Link Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   let name = "<Karl.dev />"
   return (
-    <div className='container mx-auto flex justify-between items-center px-2 bg-slate-600'>
-        <h1 className='text-3xl font-bold'> {name}</h1>
-        <ul className='flex'>
-            <li className='p-4'>Home</li>
-            <li className='p-4'>Blog</li>
-            <li className='p-4'>Projects</li>
-            <li className='p-4'>Blog</li>
-        </ul>
-        <h1>☀️</h1>
+    <div className='relative flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>{name}</h1>
+      <ul className='flex'>
+        <li className='p-4'>Home</li>
+        <Link className='p-4' href='/blog'>Blog</Link>
+        <li className='p-4'>Projects</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
     </div>
   )
 }
