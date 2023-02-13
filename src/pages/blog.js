3425a9d357eecfa3489670a58184dc4f2370d4bf
Karl Tiama <karltiama@gmail.com>
2023-02-13 15:31:26 -0500

    feat: add blog component

---
+++ src/pages/blog.js
@@ -0,0 +1,18 @@
+
+import Navbar from '../components/Navbar'
+import Head from 'next/head'
+
+export default function Blog() {
+    return (
+      <>
+        <Head>
+          <title>Karl.Dev | Blog</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Navbar/>
+        
+      </>
+    )
+  }
+  
