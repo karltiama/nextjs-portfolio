762f5d7499b76f8ed21c650c0183fd6adec28385
Karl Tiama <karltiama@gmail.com>
2023-02-20 17:11:33 -0500

    chore: made another link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='relative flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>{name}</h1>
       <ul className='flex'>
-        <li className='p-4'>Home</li>
+        <Link className='p-4' href='/'>Home</Link>
         <Link className='p-4' href='/blog'>Blog</Link>
         <li className='p-4'>Projects</li>
         <li className='p-4'>About</li>
